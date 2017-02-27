de4a7b127cc83ec38e6b6365ec4e9eb6df335a58
James <jwin4740@gmail.com>
2017-02-26 20:51:46 -0500

    closer

--- flashcard.js
+++ flashcard.js
@@ -1,5 +1,7 @@
 // dependency for inquirer npm package
 var inquirer = require("inquirer");
+// NPM Package for reading and writing files
+var fs = require("fs");
 
 // basic card constructor function
 
@@ -8,14 +10,35 @@ function BasicCard(basicFront, basicBack) {
     this.basicBack = basicBack;
 }
 
+function ClozeCard(fullText, clozePart) {
+	this.fullText = fullText;
+    this.clozePart = clozePart;
+}
 
-// Inquirer asks whether to post a bid or bid on one
+ClozeCard.prototype.getPartial = function () {
+	console.log(this.fullText);
+
+}
+
+
+
+var novoCloze = new ClozeCard("George Washington is the first president of the United States", "George Washington");
+var textString = novoCloze.fullText;
+var partialString = textString.replace(novoCloze.clozePart, "__________");
+console.log("Your formatted ClozeCard is:\n\n" + partialString);
+
+
+
+
+
+
+
 var start = function() {
     inquirer.prompt([{
         name: "card",
         type: "list",
         message: "Which type of flashcard do you want to might?",
-        choices: ["basic", "cloze", "I don't want to learn"]
+        choices: ["basic", "cloze", "quizme", "I don't want to learn"]
     }]).then(function(answer) {
 
         switch (answer.card) {
@@ -26,15 +49,19 @@ var start = function() {
             case "cloze":
                 makeClozeCard();
                 break;
+            
+            case "quizme":
+                quizMe();
+                break;
 
             case "I don't want to learn":
                 lazy();
                 break;
-                // If Post, ask item_name, item_starting_bid
+              
         }
-    }); // End of post or bid inquirer prompt
+    }); 
 }
-start();
+// start();
 
 function makeBlankCard() {
     inquirer.prompt([{
@@ -47,29 +74,36 @@ function makeBlankCard() {
             type: "input",
             message: "Type in an answer to your question"
 
+        }, {
+            name: "confirmation",
+            type: "confirm",
+            message: "Would you like to add another notecard?"
         }
 
+
     ]).then(function(answer) {
-        console.log(answer.blankquestion);
-        console.log(answer.blankanswer);
 
+        var novoBasicCard = new BasicCard(answer.blankquestion, answer.blankanswer);
+        // This block of code will create a file called "movies.txt".
+        // It will then print "Inception, Die Hard" in the file
+        fs.appendFile("basicbank.txt", JSON.stringify(novoBasicCard) + ';', function(err) {
 
-        inquirer.prompt([{
-                name: "confirmation",
-                type: "confirm",
-                message: "Would you like to add another notecard?"
+            // If the code experiences any errors it will log the error to the console.
+            if (err) {
+                return console.log(err);
             }
 
-        ]).then(function(response) {
-
-        	if (response.confirmation){
-        		makeBlankCard()
-        	}
-            else {
-            	return;
-            }
+            // Otherwise, it will print: "movies.txt was updated!"
+            console.log("movies.txt was updated!");
 
         });
+
+        if (answer.confirmation) {
+            makeBlankCard()
+        } else {
+            return;
+        }
+
     });
 }
 
@@ -85,4 +119,22 @@ function lazy() {
     console.log("too bad, sucker");
     console.log("---------------\n");
     setTimeout(start, 1000);
+}
+
+
+
+function quizMe() {
+
+    fs.readFile("basicbank.txt", "utf8", function(error, data) {
+        // the first parameter is always an error which gets sets to null if there is no error
+        // We will then print the contents of data
+   
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(";");
+
+        // We will then re-display the content as an array for later use.
+        var object = JSON.parse(dataArr[1]);
+        console.log(object.basicFront);
+
+    });
 }
